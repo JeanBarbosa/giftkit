f37cf7266458b14c03718ab0353fe616f995dcd1
jeanBarbosa <programmer.jean@gmail.com>
2023-01-23 16:37:00 -0300

    feat: add http module axios for requests

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, ForbiddenException } from '@nestjs/common'
+import { AppService } from './app.service'
+import { Observable, map, catchError } from 'rxjs'
+import { HttpService } from '@nestjs/axios'
+import { AxiosResponse } from 'axios'
+
+type Category = {
+  record_id: string,
+  public_id: string
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService
+  ) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
+  }
+
+  @Get('categories')
+  findAll(): Observable<AxiosResponse<Category[]>> {
+    return this.httpService.get('https://api.beuni.com.br/atlas/brands/v2/categories?reduced=true')
+      .pipe(
+        map((response) => {
+          return response.data
+        }),
+      )
+      .pipe(
+        catchError(() => {
+          throw new ForbiddenException('API not available')
+        }),
+      )
   }
 }
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { HttpModule, HttpService } from '@nestjs/axios'
+import { ApiModule } from './api/api.module'
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+    ApiModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
